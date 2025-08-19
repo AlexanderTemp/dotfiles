03a384ced985fba0a947999af8d59d99008584a4
Alexander Nina <alexander.nina@agetic.gob.bo>
2025-08-19 10:19:03 -0400

    fix: added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,52 @@
+if status is-interactive # Starship Prompt
+    starship init fish | source
+
+    # Aliases
+    alias g="git"
+    alias ga="git add ."
+    alias d="docker"
+    alias x="exit"
+    
+    alias t="tmux"
+    alias ii="npm install"
+    alias dev="npm run dev"
+
+    alias v="vim"
+
+    alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+    # ripgrep
+    alias rgf="rg --files | rg" 
+    alias fd="fdfind"
+
+   
+
+    # Cargar NVM
+    nvm use v20
+    
+    # path nvim 
+    alias nv="nvim"
+    
+
+    function fzf-lovely
+      set preview '[[ $(file --mime {}) =~ binary ]] &&
+      echo {} is a binary file ||
+      (batcat --style=numbers --color=always {} ||
+      highlight -O ansi -l {} ||
+      coderay {} ||
+      rougify {} ||
+      cat {}) 2> /dev/null | head -500'
+      if test "$argv[1]" = "h"
+    	fzf -m --reverse --preview-window down:20 --preview "$preview"
+      else
+    	fzf -m --preview "$preview"
+      end
+    end
+    set -gx PATH "/home/alexander/.config/herd-lite/bin" $PATH
+
+end
+
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
